83feb8b0b9fd8d2897de8913b5f5f2fdf985d543
Raphael L <oraphalmr@gmail.com>
2025-11-12 15:22:46 -0300

    adicionado assets e formatações

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("olá mundo");
+///region trabalhando com variáveis
+//fazendo uma variável para nome woaw
+let nome ="Rapha";
+console.log("Nome: ", nome);
+
+//declarando uma variável para númerows waow
+let numero = 5;
+console.log("Numero: ", numero)
+
+// fazendo um array
+let frutas = ["maça", "banana", "pepino"];
+
+console.log("lista de frutas pra comprar: ", frutas);
+console.log("lista de frutas que cauã gosta de enfiar na bunda: ", frutas[2]);
+
+//fazendo uma lista com mais de uma propriedade
+let pessoas = {nome: 'bizonho', idade: 50};
+console.log(pessoas);
+console.log(pessoas.nome);
+console.log(pessoas.idade);
+///endregion
+
+//operadores básicos
+let a = 10;
+let b = 5;
+//let c = parseInt(prompt("diga um numero para fazer as quatro operações com 10"));
+
+console.log("soma ", a+c);
+console.log("subtração ",a-c);
+console.log("mult ",a*c);
+console.log("div ",a/c);  
