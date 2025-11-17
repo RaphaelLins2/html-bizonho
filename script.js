625763995dfc7b1cfa79c134afc127a395363911
Raphael L <oraphalmr@gmail.com>
2025-11-17 18:07:24 -0300

    adicionado um botão que muda a lingua do perfil
    posteriormente, planejo colocar estas variações de linguas em um arquivo JSON. e também fazer a mudança de língua para o currículo todo

--- script.js
+++ script.js
@@ -2,6 +2,46 @@ document.getElementById("btn-print").addEventListener(
     "click", () => (window.print())
 );
 
+//chamando o arquivo JSON
+//aqui nos na verdade finjimos que estamos chamando um JSON
+
+//eu ainda nao entendi como chama um JSON :< 
+
+
+
+
+//definindo as variaveis de lingua
+let langs = [
+    "Olá meu nome é Raphael Lins, faço o ensino médio no senac Mediotec junto ao técnico de informática com previsão ainda no ano de 2025. Gosto muito de mecher com tecnologia em geral, seja programar ou mexer em hardware.",
+    "Hello my name is Raphael Lins, currently doing high school together with a technical course in IT at Senac Mediotec, that will end by 2025. I like to mess with tecnology in general, wether it's coding or messing with hardware.",
+    "おはよう私の名前はラフアエル・リンスです、今まで高等学校はセナックでします、それにテクニカルコースもまた。私はテクノロジーを好きです。"]
+let lang = 0
+let perfilTxt = document.getElementById("perfilTxt")
+//funcionalidade de mudar a lingua do perfil
+function mudarLingua(linguas){
+    //console.log(lang, " ", linguas[lang])
+
+    // faz um loop onde a lingua varia entre 3 diferentes variáveis
+    if (lang <2){
+        lang++;
+        console.log(lang, " num seletor. a lingua atual é: ", linguas[lang] )
+    }else{
+        lang = 0;
+        console.log("seletor setado para 0")
+    }
+    perfilTxt.textContent = linguas[lang]
+
+}
+
+//funcionalidade do botão para mudar a lingua que chama a função de mudar lingua
+document.getElementById("btn-lang").addEventListener(
+    "click", () => {
+        mudarLingua(langs, lang)
+        console.log("botão clicado")
+    }
+);
+
+
 let imagem = document.getElementById("bottom-image");
 
 let dirPath = "./Assets/";
