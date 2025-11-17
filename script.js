89bb4b287e324379c3df1b5983bc55858349255a
Raphael L <oraphalmr@gmail.com>
2025-11-17 15:24:03 -0300

    add json file for diff langs

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ document.getElementById("btn-print").addEventListener(
 
 let imagem = document.getElementById("bottom-image");
 
-imagem.src = "./Assets/aura.png";
+let dirPath = "./Assets/";
+
+let imagens = [
+    "aura.png",
+    "chudking.png",
+    "yuri.jpeg",
+    "FlowersAndMusketeerFG.png",
+    "FlyingHogPixelFG.png",
+    "HogAndMusketeerJourneyFG.png",
+    "JojoMusketeerFG.png",
+    "KawaiMusketeersFG.png",
+    "MiniPEKKACard.png",
+    "PorcoFG.png"    
+]
+
+let randomImageSelector = Math.floor(Math.random() * imagens.length);
+
+imagem.src = dirPath + imagens[randomImageSelector];
