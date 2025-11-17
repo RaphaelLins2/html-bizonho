90456f523ec7b22efc696bde1e70541676684bd7
Raphael <oraphalmr@gmail.com>
2025-11-17 08:19:58 -0300

    experimentando com javascript

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("btn-print").addEventListener(
     "click", () => (window.print())
 );
+
+let imagem = document.getElementById("bottom-image");
+
+imagem.src = "./Assets/aura.png";
